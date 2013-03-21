3782b98abb4e45e0bda34a9c53fdeffbbc09e0ed
Moshe Vilner <shootermv@gmail.com>
2013-03-21 19:01:52 +0200

    part3
    getting all the stuff together

--- js/tabsViewModel.js
+++ js/tabsViewModel.js
@@ -1,26 +1,26 @@
-var tabViewModel=function(title, active, content){ 
- this.title=ko.observable(title);
- this.active=ko.observable(active);
- this.content=ko.observable(content);
+var tabViewModel=function(title, active, model, templateName){ 
+  this.title=ko.observable(title);
+  this.active=ko.observable(active);
+  this.model=ko.observable(model);
+  this.templName=templateName;
 };
 var tabsViewModel=function(){  
-  this.tabsArr=ko.observableArray([]); 
+  this.tabsArr=ko.observableArray([]);//new tabViewModel('home','active', new homeViewModel(),'homeTmpl'),new tabViewModel('search','' ,new searchViewModel(),'searchTmpl')]) ; 
+
+ 
   this.makeTabActive=function(data){
-     //make all the tabs unactive
-     $.each(this.tabsArr(),function(ind,item){
+    
+      $.each(this.tabsArr(),function(ind,item){
 	      item.active('');
-	 });
-     //activate current tab	 
-     data.active('active');	 
+	  });
+	  
+     data.active('active');
+	 
   }.bind(this);
   
-  
-  this.addTab=function(title, active, content){
-    var nt=new tabViewModel(title, active, content);
+  this.addTab=function(title, active, model, template){
+    var nt=new tabViewModel(title, active, model, template);
 	this.tabsArr.push(nt);
     this.makeTabActive(nt);
   }.bind(this);
 };
-var tabs=new tabsViewModel();
-tabs.addTab('home','active', 'This is a home tab')
-ko.applyBindings(tabs);
